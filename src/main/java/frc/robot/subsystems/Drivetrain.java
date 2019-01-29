a3f2ce70a89c6e72b3d32555bece4a49bb17e04b
johnnybegood1318 <johnnybegood1414@gmail.com>
2019-01-28 23:40:25 -0500

    update Drivetrain for proper command project coding practices

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,17 +9,14 @@ import frc.robot.RobotMap;
 
 public class DriveTrain extends Subsystem {
 
-    private WPI_TalonSRX frontRight;
-    private WPI_TalonSRX frontLeft;
-    private WPI_TalonSRX backRight;
-    private WPI_TalonSRX backLeft;
+    private WPI_TalonSRX frontRight = null;
+    private WPI_TalonSRX frontLeft = null;
+    private WPI_TalonSRX backRight = null;
+    private WPI_TalonSRX backLeft = null;
 
-    private MecanumDrive driveTrain;
+    private MecanumDrive driveTrain = null;
 
-
-    @Override
-    protected void initDefaultCommand() {
-        
+    public DriveTrain() {
         /* The declaration numbers are to keep track of which motor controller is which.
          * Using the RobotMap class reduces the number of unexplained numbers (also called magic numbers)
          * in the code. This applies for all sorts of things besides motor controllers.
@@ -28,10 +25,13 @@ public class DriveTrain extends Subsystem {
         frontLeft = new WPI_TalonSRX(RobotMap.FRONT_LEFT);
         backRight = new WPI_TalonSRX(RobotMap.BACK_RIGHT);
         backLeft = new WPI_TalonSRX(RobotMap.BACK_LEFT); 
-
         
-    	driveTrain = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
-    	
+        driveTrain = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new frc.robot.commands.MecanumDrive());    	
     }
 
     public void move(double yAxis, double xAxis, double zRotation) {
